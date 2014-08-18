a326a1f981b75f5ca3f36538bc0dec4901875155
Agno Almario <agno@adarna.com.ph>
2014-08-18 17:07:37 +0800

    page template

--- wp-content/themes/agfitheme/front-page.php
+++ wp-content/themes/agfitheme/front-page.php
@@ -18,16 +18,7 @@ get_header(); ?>
 
 
 <!-- navbar -->
-		<nav id="navigation" class="grid-12">
-			<ul>
-				<li><a href="<?php bloginfo('url'); ?>"> Home </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Rationale"> Rationale </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Unang Aklat"> Unang Aklat </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Partners"> Partners </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/News"> News </a></li>
-				<li><a class="last-item" href="<?php bloginfo('url'); ?>/News"> Contact Us </a></li>
-			</ul>
-		</nav>
+<?php get_template_part(nav,menu); ?>
 
 <!--  -->
 
--- wp-content/themes/agfitheme/nav-menu.php
+++ wp-content/themes/agfitheme/nav-menu.php
@@ -1,9 +1,10 @@
-		<nav>
+		<nav id="navigation" class="grid-12">
 			<ul>
-				<!-- <li><a href="<?php bloginfo('url'); ?>"> Home </a></li> -->
-				<li><a href="<?php bloginfo('url'); ?>/products"> Products </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/blog"> Blog </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/about"> About </a></li>
-				<li><a href="https://adarnadigital.desk.com/"> Support </a></li>
+				<li><a href="<?php bloginfo('url'); ?>"> Home </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/Rationale"> Rationale </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/Unang Aklat"> Unang Aklat </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/Partners"> Partners </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/News"> News </a></li>
+				<li><a class="last-item" href="<?php bloginfo('url'); ?>/News"> Contact Us </a></li>
 			</ul>
 		</nav>
