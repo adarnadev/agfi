6036a470236b970b24e38b79c0e48e8ce622ed1d
Agno Almario <agno@adarna.com.ph>
2014-08-18 17:24:20 +0800

    nav menu

--- wp-content/themes/agfitheme/nav-menu.php
+++ wp-content/themes/agfitheme/nav-menu.php
@@ -1,10 +1,10 @@
 		<nav id="navigation" class="grid-12">
 			<ul>
 				<li><a href="<?php bloginfo('url'); ?>"> Home </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Rationale"> Rationale </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Unang Aklat"> Unang Aklat </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/Partners"> Partners </a></li>
-				<li><a href="<?php bloginfo('url'); ?>/News"> News </a></li>
-				<li><a class="last-item" href="<?php bloginfo('url'); ?>/News"> Contact Us </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/rationale"> Rationale </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/unang-aklat"> Unang Aklat </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/partners"> Partners </a></li>
+				<li><a href="<?php bloginfo('url'); ?>/news"> News </a></li>
+				<li><a class="last-item" href="<?php bloginfo('url'); ?>/contact"> Contact Us </a></li>
 			</ul>
 		</nav>
